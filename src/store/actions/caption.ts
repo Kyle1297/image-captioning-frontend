1020671d0ca33d5e2476251107042de61978029b
Kyle1297 <kyle.griffin.1297@gmail.com>
2021-06-02 05:29:47 +1000

    allow private caption to be updated

--- src/store/actions/caption.ts
+++ src/store/actions/caption.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from 'redux';
 import { RequestEnums } from '../types/request';
-import { requestHelper, MethodTypes } from './actionUtils';
+import { requestHelper, MethodTypes, authRequestHeaders } from './actionUtils';
 import {
 	UPDATE_CAPTION,
 	CaptionPatch,
@@ -8,12 +8,14 @@ import {
 	SET_CAPTION,
 } from '../types/caption';
 import { SET_CAPTION_IN_IMAGES } from '../types/image';
+import { AppState } from '../reducers';
 
 const API_SERVER = process.env.REACT_APP_API_SERVER + '/images/captions';
 
 // UPDATE CAPTION
 export const updateCaption =
-	(id: number, data: CaptionPatch) => (dispatch: Dispatch) => {
+	(id: number, data: CaptionPatch) =>
+	(dispatch: Dispatch, getState: () => AppState) => {
 		requestHelper({
 			dispatch: dispatch,
 			requestName: RequestEnums.updateCaption,
@@ -21,6 +23,7 @@ export const updateCaption =
 			requestConfig: {
 				url: `${API_SERVER}/${id}/`,
 				method: MethodTypes.PATCH,
+				headers: authRequestHeaders(getState().auth.token),
 				data: data,
 			},
 			extraAction: SET_CAPTION_IN_IMAGES,
